79aff64719207fc68df53a6ffe5607c2cf271fcd
gingin33 <82872408+gingin33@users.noreply.github.com>
2022-01-18 16:28:24 +0900

    [ADD] index.js style.css [EDIT] getSMDTweets.php
    ツイートを取得してマップ表示させるようにさせました。まだピンたてはできてない

---
+++ index.js
@@ -0,0 +1,8 @@
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 35.183961197271, lng: 137.11132629433  },
+    zoom: 8,
+    });
+}
